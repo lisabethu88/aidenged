abff561f6972a2f3da5e7b0a40f884a6a0749aac
Lisa Utsett <lisabethutsett@gmail.com>
2023-10-09 16:13:40 -0400

    updated hw

--- aiden-ged/src/Pages/Home.js
+++ aiden-ged/src/Pages/Home.js
@@ -14,37 +14,69 @@ export const Home = () => {
           Homework {d.getMonth() + 1}/{d.getDate()}/{d.getFullYear()}
         </h2>
         <section className="board">
-          <p>
-            <b>NOTE:</b> These assignments will be due Tuesday evening by
-            7:00pm.
-          </p>
           <h3>Khan Academy - Science</h3>
           <p>
             <b>Instructions:</b> Watch the video, read review, and do quiz.
             Write down the results and share with Titi.
           </p>
-          <ol>
+          <ul>
             <a href="https://www.khanacademy.org/science/high-school-biology/hs-cells/hs-plant-vs-animal-cells/v/overview-of-animal-and-plant-cells">
               <li>Khan Academy - Plant and Animal Cells</li>
             </a>
-          </ol>
+            <li>
+              <em>
+                <b>DUE:</b> Oct 9th by 8pm
+              </em>
+            </li>
+          </ul>
           <h3>Onsego - Science </h3>
-          <ol>
+          <ul>
+            <li>
+              <em>
+                <b>DUE:</b> Oct 9th by 8pm
+              </em>
+            </li>
             <li>Photosynthesis</li>
             <li>The Top Four Kingdoms </li>
             <li>Organization of Life Practice Test </li>
             <li>Organ Systems </li>
             <li>Nervous System </li>
             <li>The Circulatory System </li>
-          </ol>
+          </ul>
+          <h3>Language Arts (Writing)</h3>
+          <ul>
+            <li>
+              <em>
+                <b>DUE:</b> Oct 9th by 8pm
+              </em>
+            </li>
+            <li>Do a prompt in essay book.</li>
+          </ul>
           <h3>Onsego - History</h3>
-          <ol>
+          <ul>
+            <li>
+              <em>
+                <b>DUE:</b> Oct 10th by 8pm
+              </em>
+            </li>
             <li>The Judicial Branch</li>
             <li> The Supreme Court</li>
             <li>State and Local Government</li>
             <li>Voting and Elections</li>
             <li>Political Parties and Politics</li>
-          </ol>
+          </ul>
+          <h3>Onsego - Language Arts</h3>
+          <ul>
+            <li>
+              <em>
+                <b>DUE:</b> Oct 10th by 8pm
+              </em>
+            </li>
+            <li>Parts of Speech</li>
+            <li>Subjects and Verbs</li>
+            <li>Pronoun Agreement</li>
+            <li>Independent and Dependent Clauses</li>
+          </ul>
         </section>
       </section>{" "}
       <img width="500px" src={braixen} alt="braixen" />
